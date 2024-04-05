58b79da48efd812745f838c27d69f658b46c8392
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2024-04-06 01:10:09 +0900

    feat : 프로젝트 초기 설정

---
+++ src/test/java/com/unit/whisper/WhisperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit.whisper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhisperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
