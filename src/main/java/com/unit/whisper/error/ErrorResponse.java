23ef3d1d6bd3f15a5ffb1e0be8e5ef6bd4689eab
GaBaljaintheroom <junsu1222@naver.com>
2024-04-06 19:46:21 +0900

    feat: 예오 처리 로직 추가

---
+++ src/main/java/com/unit/whisper/error/ErrorResponse.java
@@ -0,0 +1,66 @@
+package com.unit.whisper.error;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+@Schema(description = "에러 발생 시 응답")
+public record ErrorResponse(
+        @Schema(description = "에러 코드") String code,
+        @Schema(description = "에러 메시지") String message,
+        @Schema(description = "에러 리스트 ex) 필드 에러들") List<Error> result) {
+
+    public static ErrorResponse fromErrorCode(final ErrorCode errorCode) {
+        return new ErrorResponse(
+                errorCode.getCode(), errorCode.getMessage(), Collections.emptyList());
+    }
+
+    public static ErrorResponse ofBindingResult(
+            final ErrorCode errorCode, final BindingResult bindingResult) {
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorCode.getMessage(),
+                Error.fromBindingResult(bindingResult));
+    }
+
+    public static ErrorResponse fromParameter(
+            final ErrorCode errorCode, final String parameterName) {
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorCode.getMessage(),
+                List.of(Error.fromParameter(parameterName)));
+    }
+
+    public static ErrorResponse fromType(
+            final ErrorCode errorCode, final String parameterName, final String value) {
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorCode.getMessage(),
+                List.of(Error.fromType(parameterName, value)));
+    }
+
+    public record Error(String field, String value, String reason) {
+
+        public static Error fromType(final String parameterName, final String value) {
+            return new Error(parameterName, value, "입력 파라미터의 타입이 올바르지 않습니다.");
+        }
+
+        public static Error fromParameter(final String parameterName) {
+            return new Error(parameterName, null, "필수 입력 파라미터를 포함하지 않았습니다.");
+        }
+
+        public static List<Error> fromBindingResult(final BindingResult bindingResult) {
+            return bindingResult.getFieldErrors().stream().map(Error::fromFieldError).toList();
+        }
+
+        private static Error fromFieldError(final FieldError fieldError) {
+            return new Error(
+                    fieldError.getField(),
+                    String.valueOf(fieldError.getRejectedValue()),
+                    fieldError.getDefaultMessage());
+        }
+    }
+}
