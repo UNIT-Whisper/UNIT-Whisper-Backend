b17b07722c558b4ea20c7455a9a97e3877124669
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2024-04-06 11:00:14 +0900

    style : spotless 적용

--- src/main/java/com/unit/whisper/config/WebMvcConfig.java
+++ src/main/java/com/unit/whisper/config/WebMvcConfig.java
@@ -1,12 +1,11 @@
 package com.unit.whisper.config;
 
 
+import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.ArrayList;
 
 @Configuration
 @RequiredArgsConstructor
