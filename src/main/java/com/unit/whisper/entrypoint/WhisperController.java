8a0e325e7b4e4043bb5fa11307b2d4d12b1b0d3f
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2024-04-07 06:36:47 +0900

    docs : API 명세 수정

--- src/main/java/com/unit/whisper/entrypoint/WhisperController.java
+++ src/main/java/com/unit/whisper/entrypoint/WhisperController.java
@@ -18,7 +18,7 @@ public class WhisperController {
     private final WhisperService whisperService;
 
     @PostMapping
-    @Operation(summary = "속삭임 생성", description = "사용자들의 속삭임을 저장한다.")
+    @Operation(summary = "구름 생성", description = "사용자들의 속삭임을 저장한다.")
     public CustomResponseEntity<Long> createWhisper(
             @Valid @RequestBody WhisperCreateRequest request) {
         whisperService.saveWhisper(request);
@@ -26,6 +26,7 @@ public class WhisperController {
         return CustomResponseEntity.success(whisperService.saveWhisper(request));
     }
 
+    @Operation(summary = "구름 조회", description = "사용자들의 저장한 구름 정보를 알림 목록에서 상세 조회한다.")
     @GetMapping("/{whisperId}")
     public CustomResponseEntity<WhisperResponse> getWhisper(@PathVariable Long whisperId) {
         return CustomResponseEntity.success(whisperService.getWhisper(whisperId));
