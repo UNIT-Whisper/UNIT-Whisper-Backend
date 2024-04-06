3d2eb190f173cab232e11b6f40aafaaf72727423
GaBaljaintheroom <junsu1222@naver.com>
2024-04-06 13:59:13 +0900

    fix: spotless 적용

--- src/main/java/com/unit/whisper/config/SwaggerConfig.java
+++ src/main/java/com/unit/whisper/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.unit.whisper.config;
 
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -15,33 +16,26 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
-            .servers(getServers())
-            .info(getInfo())
-            .components(new Components()
-                .addSecuritySchemes("user_token", tokenSecuritySchema())
-            );
+                .servers(getServers())
+                .info(getInfo())
+                .components(
+                        new Components().addSecuritySchemes("user_token", tokenSecuritySchema()));
     }
 
     private List<Server> getServers() {
-        return List.of(
-            new Server()
-                .url("/")
-                .description("백엔드 api 서버")
-        );
+        return List.of(new Server().url("/").description("백엔드 api 서버"));
     }
 
     private Info getInfo() {
-        return new Info().title("Whisper API")
-            .description("나만의 감정 별마당")
-            .version("v1");
+        return new Info().title("Whisper API").description("나만의 감정 별마당").version("v1");
     }
 
     public SecurityScheme tokenSecuritySchema() {
         return new SecurityScheme()
-            .name("Authorization")
-            .scheme("bearer")
-            .bearerFormat("JWT")
-            .in(SecurityScheme.In.HEADER)
-            .type(SecurityScheme.Type.HTTP);
+                .name("Authorization")
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .type(SecurityScheme.Type.HTTP);
     }
 }
