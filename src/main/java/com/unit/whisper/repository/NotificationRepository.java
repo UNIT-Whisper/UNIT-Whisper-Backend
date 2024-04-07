2108cdb640c7af4578987bef41fa4bdb34aa99b2
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2024-04-07 09:47:32 +0900

    refact : 조회 수정

--- src/main/java/com/unit/whisper/repository/NotificationRepository.java
+++ src/main/java/com/unit/whisper/repository/NotificationRepository.java
@@ -4,10 +4,13 @@ package com.unit.whisper.repository;
 import com.unit.whisper.domain.notification.Notification;
 import com.unit.whisper.domain.notification.NotificationStatus;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     List<Notification> findAllByUserIdAndWhisperIdAndNotificationStatus(
             Long userId, Long whisperId, NotificationStatus notificationStatus);
+
+    Optional<Notification> findByIdAndWhisperId(Long notificationId, Long whisperId);
 }
